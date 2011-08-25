b008cd1b163bb7e95639f332adf1c9b9442bd899
liam.greenhughes <liam.greenhughes@localhost>
2011-08-25 23:46:22 +0100

    Converted Preferences activity to be fragments based. Also changed default values to optimise for tablet use.

--- android/ScriptingLayerForAndroid/src/com/googlecode/android_scripting/activity/Preferences.java
+++ android/ScriptingLayerForAndroid/src/com/googlecode/android_scripting/activity/Preferences.java
@@ -16,18 +16,68 @@
 
 package com.googlecode.android_scripting.activity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
 
-import com.googlecode.android_scripting.Analytics;
 import com.googlecode.android_scripting.R;
+import com.googlecode.android_scripting.Version;
+
+import java.util.List;
 
 public class Preferences extends PreferenceActivity {
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    // Load the preferences from an XML resource
-    addPreferencesFromResource(R.xml.preferences);
-    Analytics.trackActivity(this);
+    ActionBar actionBar = getActionBar();
+    actionBar.setTitle("Preferences");
+    actionBar.setSubtitle("Scripting Layer for Android r" + Version.getVersion(this));
+    actionBar.setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int itemId = item.getItemId();
+
+    // action bar back
+    if (itemId == android.R.id.home) {
+      Intent intent = new Intent(this, ScriptManager.class);
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+      startActivity(intent);
+      finish();
+    }
+    return true;
+  }
+
+  /**
+   * Populate the activity with the top-level headers.
+   */
+  @Override
+  public void onBuildHeaders(List<Header> target) {
+    loadHeadersFromResource(R.xml.preference_headers, target);
+  }
+
+  public static class PreferencesGeneral extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      // Load the preferences from an XML resource
+      addPreferencesFromResource(R.xml.preferences_general);
+      // Analytics.trackActivity(this);
+    }
+  }
+
+  public static class PreferencesTerminal extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      // Load the preferences from an XML resource
+      addPreferencesFromResource(R.xml.preferences_terminal);
+      // Analytics.trackActivity(this);
+    }
   }
 }
